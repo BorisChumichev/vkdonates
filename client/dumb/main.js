9a31afd1ce7dfd4220db8bfc9f1354738189d7ed
Chmichev Boris <boris362@yandex.ru>
2016-11-26 16:25:15 +0300

    Tiny fix form payment form stylings

---
+++ client/dumb/main.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Paper from 'dumb/paper'
+import { partial } from 'ramda'
+import MainHeading from 'dumb/main-heading'
+import Button from 'dumb/button'
+import ButtonNest from 'dumb/button-nest'
+import Stat from 'dumb/stat'
+import StatsRow from 'dumb/stats-row'
+import TabsControl from 'dumb/tabs-control'
+import Income from 'dumb/income'
+import Message from 'dumb/message'
+
+class SettingsForm extends Component {
+  render() {
+    const { sss } = this.props
+
+    return (
+      <div>
+        <Paper>
+        <MainHeading
+          title={"Благотворительный фонд «ПОДАРИ ЖИЗНЬ»"}
+          avatarURL={"https://cs7051.vk.me/c626128/v626128515/2891b/NxH6b4w-XKs.jpg"}
+          />
+        <ButtonNest>
+          <Button action={() => console.log('click')}>Сделать взнос</Button>
+          <Button isEmpty={true} action={() => console.log('click2')}>Цели сборов</Button>
+        </ButtonNest>
+        <StatsRow>
+          <Stat value={311} caption={'взносов'}/>
+          <Stat value={139} caption={'спонсоров'}/>
+          <Stat value={149000} caption={'привлечено'} isCurrency={true}/>
+          <Stat value={523} caption={'сред. взнос'} isCurrency={true}/>
+        </StatsRow>
+      </Paper>
+      <TabsControl
+        tabs={[ 'Последние', 'Рейтинг' ]}
+        current={'Последние'}
+        action={tab => console.log(tab)}
+        />
+      <Income id="78195752" name="Evgeny Terskikh" value="500" avatarURL="https://pp.vk.me/c626829/v626829622/37ff/9edmvCaf2t4.jpg" date={Date.now() - 120000} />
+      <Income id="78195752" name="Inkignito" value="1500" avatarURL={null} date={Date.now() - 12000000} />
+      <Income id="78195752" name="Inkignito" value="1500" avatarURL={null} place={1} />
+      <Message>Никто еще не совршал пожертвований, вы можете стать первым!</Message>
+    </div>
+    )
+  }
+}
+
+export default SettingsForm
