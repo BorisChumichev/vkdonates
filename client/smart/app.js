95207241f8201f0626a8ee0a7534b943f150044a
Chmichev Boris <boris362@yandex.ru>
2016-11-27 07:18:36 +0300

    fix for incognit inversion

--- client/smart/app.js
+++ client/smart/app.js
@@ -39,7 +39,7 @@ class App extends Component {
     }&form-comment=${
       encodeURIComponent(`Пожертвование для ${group.name}`)
     }&short-dest=Пожертвование%20Вконтакте&paymentType=AC&quickpay-form=donate&quickpay-back-url=https%3A%2F%2Fvk.com%2Fvkdonates&successURL=https%3A%2F%2Fvk.com%2Fvkdonates&shop-host=vkdonates.com&targets=Пожертвование%20Вконтакте&label=${
-      options.incognito ? user.user_id : 0
+      options.incognito ? 0 : user.user_id
     }&comment=`
   }
 
--- server/boot/root.js
+++ server/boot/root.js
@@ -71,6 +71,7 @@ module.exports = function(app) {
       .findOne({ where: { group_id: req.params.group_id } })
       .then(group => {
         const notification_secret = group.secret
+        console.log('LABEl', req.body.label)
         const checkString = `${req.body.notification_type}&${req.body.operation_id}&${req.body.amount}&${req.body.currency}&${req.body.datetime}&${req.body.sender}&${req.body.codepro}&${notification_secret}&${req.body.label}`
         const checkSum = sha1(checkString)
         console.log(checkSum, req.body.sha1_hash)
