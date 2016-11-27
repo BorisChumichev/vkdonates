117cca1df99f237a1d8cc8cad58bad825931cf50
Chmichev Boris <boris362@yandex.ru>
2016-11-27 05:24:08 +0300

    Ooooh!

--- server/boot/root.js
+++ server/boot/root.js
@@ -52,22 +52,19 @@ module.exports = function(app) {
       .findOne({ where: { group_id: req.params.group_id } })
       .then(group => {
         const notification_secret = group.secret
-        const checkString = `${req.query.notification_type}&${req.query.operation_id}&${req.query.amount}&${req.query.currency}&${req.query.datetime}&${req.query.sender}&${req.query.codepro}&${notification_secret}&${req.query.label}`
+        const checkString = `${req.body.notification_type}&${req.body.operation_id}&${req.body.amount}&${req.body.currency}&${req.body.datetime}&${req.body.sender}&${req.body.codepro}&${notification_secret}&${req.body.label}`
         const checkSum = sha1(checkString)
-        console.log(checkSum, req.query.sha1_hash)
-        if (checkSum !== req.query.sha1_hash) return res.status(401).end()
+        console.log(checkSum, req.body.sha1_hash)
+        if (checkSum !== req.body.sha1_hash) return res.status(401).end()
         app.models.Income.create({
-          user_id: req.query.label,
-          amount: req.query.amount,
+          user_id: req.body.label,
+          amount: req.body.amount,
           group_id: req.params.group_id,
-          date: new Date(req.query.datetime)
+          date: new Date(req.body.datetime)
         }).then(income => {
           console.log('CREATED INCOME', income)
         })
       })
-    
-
-
 
     res.status(200).end()
   });
