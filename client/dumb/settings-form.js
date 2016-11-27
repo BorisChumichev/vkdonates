09c5f910f39b9340c60cd2d80c68f02718b4c15d
Chmichev Boris <boris362@yandex.ru>
2016-11-27 07:11:56 +0300

    Blur for settings form

--- client/dumb/settings-form.js
+++ client/dumb/settings-form.js
@@ -30,7 +30,11 @@ class SettingsForm extends Component {
           <input ref="secret" className="settingsForm-input" type="password" placeholder={initialSetup ? 'Ключ уведомлений' : 'Сменить ключ'} />
         </div>
         <input className="settingsForm-submit" type="submit" value="Сохранить" />
-        {!initialSetup && <div onClick={onClose} className="settingsForm-back">Вернуться назад</div> }
+        {!initialSetup && <div onClick={() => {
+          this.refs.wallet.blur()
+          this.refs.secret.blur()
+          onClose()
+        }} className="settingsForm-back">Вернуться назад</div> }
         <div className="settingsForm-tutorial">
           Чтобы получить <strong>ключ уведомлений</strong> подключите HTTP уведомления в сервисе Яндекс.Деньги. Для этого зайдите в <strong>Яндекс.Деньги → Настройки → Всё остальное</strong> и нажмите <strong>«подключить»</strong> напротив опции <strong>«Уведомления»</strong>. Следуйте инструкциям, в качестве адреса уведомлений укажите: <strong>https://vkdonates.ru/notify/{groupId}</strong>.
         </div>
