af142a315147294b666b3fd706477fdf2b0b35a1
Chmichev Boris <boris362@yandex.ru>
2016-12-17 15:38:42 +0300

    Yandex Money notification handling fix

--- server/middleware/handleYMNotification.js
+++ server/middleware/handleYMNotification.js
@@ -1,24 +1,28 @@
+'use strict';
+
+const sha1 = require('sha1')
+
 module.exports = app => (req, res, next) => {
     //if (req.body.operation_id === 'test-notification') return res.status(200).end()
 
     app.models.Group
       .findOne({ where: { group_id: req.params.group_id } })
       .then(group => {
-        const checkString =
-          [ req.body.notification_type
-          , req.body.operation_id
-          , req.body.amount
-          , req.body.currency
-          , req.body.datetime
-          , req.body.sender
-          , req.body.codepro
-          , group.secret
-          , req.body.label
-          ].join('&')
-        const checkSum = sha1(checkString)
-        
-        console.log(checkSum, req.body.sha1_hash)
-        
+        if (!group) return res.status(404).end()
+
+        const checkSum = sha1(
+            [ req.body.notification_type
+            , req.body.operation_id
+            , req.body.amount
+            , req.body.currency
+            , req.body.datetime
+            , req.body.sender
+            , req.body.codepro
+            , group.secret
+            , req.body.label
+            ].join('&')
+          )
+
         if (checkSum !== req.body.sha1_hash) return res.status(401).end()
 
         app.models.Income.create({
@@ -30,5 +34,7 @@ module.exports = app => (req, res, next) => {
           console.log('CREATED INCOME', income)
           res.status(200).end()
         })
+
       })
+      .catch(next)
   }
