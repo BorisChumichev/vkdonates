5cafad98fc1e1bdcc9b74e6d25f06475db4b79e4
Chmichev Boris <boris362@yandex.ru>
2016-12-28 13:48:58 +0300

    Migration script

---
+++ server/create-tables.js
@@ -0,0 +1,8 @@
+var server = require('./server')
+var ds = server.dataSources.postgresDS
+var tables = ['Group', 'Income']
+ds.automigrate(tables, function(er) {
+  if (er) throw er
+  console.log('Loopback tables [' + tables + '] created in ', ds.adapter.name)
+  ds.disconnect()
+})
