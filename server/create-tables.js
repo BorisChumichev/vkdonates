f33e9361eda11d65c2b0ed87eac48f6201e513b3
Chmichev Boris <boris362@yandex.ru>
2016-12-28 14:00:39 +0300

    Relation nammes fix for migrations

--- server/create-tables.js
+++ server/create-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server')
 var ds = server.dataSources.postgres
-var tables = ['Group', 'Income']
+var tables = ['group', 'income']
 ds.automigrate(tables, function(er) {
   if (er) throw er
   console.log('Loopback tables [' + tables + '] created in ', ds.adapter.name)
