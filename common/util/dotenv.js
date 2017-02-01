0bc3a86d24697ac55fd43c5e3a10bc829b3ea0a0
Chmichev Boris <boris362@yandex.ru>
2017-02-01 15:19:10 +0300

    Make proxy for dotenv module

---
+++ common/util/dotenv.js
@@ -0,0 +1,26 @@
+// monkey patching dotenv’s config method so that
+// it accepts list of required variables and throws
+// an error if required variable is missing.
+// returns a proxy object that targets to dotenv.
+
+const { keys, without, isEmpty, not } = require('ramda')
+  
+  , errorMessage = vars =>
+    `${vars.join(',')} must be defined for your environment.`
+
+  , createProxy = dotenv => {
+      const configOriginal = dotenv.config
+
+      dotenv.config = requiredVariables => {
+        configOriginal.call(dotenv)
+
+        const missedRequiredVars = without(keys(process.env), requiredVariables)
+        
+        if (not(isEmpty(missedRequiredVars)))
+          throw new Error(errorMessage(missedRequiredVars))
+      }
+
+      return dotenv
+    }
+
+module.exports = createProxy(require('dotenv'))
